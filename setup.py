340d957a793e71f43df62f8c1c9d5972bd1d3967
vvillin <vvillin@hotmail.fr>
2024-12-16 15:27:00 +0100

    requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     "ray==2.9.0",
     "ray[tune]",
     "gymnasium==1.0.0",
-    "tensorflow==2.18.1",
+    "tensorflow==2.18.0",
     "tensorflow[and-cuda]",
     "tf_keras==2.18.0",
     "tensorflow-probability==0.24.0",
