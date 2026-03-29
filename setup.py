c8c1a6435e2e5058ca4de0eff2f0121e7a26a994
villinvic <vvillin@hotmail.fr>
2026-03-29 13:13:14 +0200

    updated dependencies

--- setup.py
+++ setup.py
@@ -10,11 +10,9 @@ setup(
     install_requires=[
     "numpy==2.0.2",
     "dm_tree",
-    "ray==2.9.0",
+    "ray>=2.9.0",
     "ray[tune]",
     "gymnasium==1.0.0",
-    "tensorflow==2.18.0",
-    "tensorflow[and-cuda]",
     "tf_keras==2.18.0",
     "tensorflow-probability==0.24.0",
     "dm-sonnet==2.0.2",
@@ -24,5 +22,9 @@ setup(
     "plotly",
     "scipy",
     "tqdm",
-    ]
+    ],
+    extras_require={
+        "gpu": ["tensorflow[and-cuda]==2.18.0"]
+    }
 )
+
